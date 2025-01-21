c6ddec8117f30b1ec2f418e9a47e6e7847d8af17
veronic28 <156885216+AshurovMagamed@users.noreply.github.com>
2025-01-21 12:38:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> points = new List<Point>
+        {
+            new Point(5, 10),
+            new ColoredPoint(20, 30, "Красный"),
+            new Trehmernaya(50, 60, 70)
+        };
+            foreach (var point in points)
+            {
+                point.Show();    
+                point.Draw();   
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
